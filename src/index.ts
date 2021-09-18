89e598abaf01324c72f3e349219ec4befa5b66aa
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-09-18 21:03:20 +0530

    UC1: Attendance Check

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 console.log("Welcome to Employee Wage Program");
 
 type EmpCheck = {
-    is_Absent: number,
-    empCheck: number
+  is_Absent: number;
+  empCheck: number;
 };
 
 const user: EmpCheck = {
-    is_Absent: 0,
-    empCheck: Math.floor(Math.random()* 10)%2
-}
+  is_Absent: 0,
+  empCheck: Math.floor(Math.random() * 10) % 2,
+};
 
-if(user.empCheck == user.is_Absent){
-    console.log("Employee is Absent")
-}
-else{
-    console.log("Employee is Present")
+if (user.empCheck == user.is_Absent) {
+  console.log("Employee is Absent");
+} else {
+  console.log("Employee is Present");
 }
